253ff87979e11803276c28c6fe235dff64de0e63
hyeyeongAn <mybymine64@naver.com>
2024-11-07 19:51:53 +0900

    UI,GETMapping

---
+++ src/main/java/com/LottoWeb/LuckyLotto/LuckyLottoApplication.java
@@ -0,0 +1,13 @@
+package com.LottoWeb.LuckyLotto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuckyLottoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LuckyLottoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/LottoWeb/LuckyLotto/LuckyLottoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LottoWeb.LuckyLotto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuckyLottoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
