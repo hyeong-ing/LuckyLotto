c448a62478b51f817b52e2a59b589b46620bdad7
hyeyeongAn <mybymine64@naver.com>
2024-11-14 17:36:49 +0900

    GetMapping -> PostMapping

--- src/main/java/com/LottoWeb/LuckyLotto/Controller/Random1.java
+++ src/main/java/com/LottoWeb/LuckyLotto/Controller/Random1.java
@@ -3,6 +3,7 @@ package com.LottoWeb.LuckyLotto.Controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.*;
 
@@ -17,7 +18,7 @@ public class Random1 {
     }
 
     @GetMapping("/Random1")
-    public String randomPage1(Model model) {
+    public String random1(Model model) {
 
         Set<Integer> lottoSet = new TreeSet<>();
 
@@ -40,7 +41,34 @@ public class Random1 {
         model.addAttribute("Number6",lottoList.get(5));
 
 
-        return "button2";
+        return "button1";
+    }
+
+    @PostMapping("/Random1")
+    public String updateRandom(Model model) {
+
+        Set<Integer> lottoSet = new TreeSet<>();
+
+        while(true) {
+            int ranNum = rd.nextInt(45)+1;
+            lottoSet.add(ranNum);
+
+            if(lottoSet.size() == 6) {
+                break;
+            }
+        }
+
+        List<Integer> lottoList = new ArrayList<>(lottoSet);
+
+        model.addAttribute("Number1",lottoList.get(0));
+        model.addAttribute("Number2",lottoList.get(1));
+        model.addAttribute("Number3",lottoList.get(2));
+        model.addAttribute("Number4",lottoList.get(3));
+        model.addAttribute("Number5",lottoList.get(4));
+        model.addAttribute("Number6",lottoList.get(5));
+
+
+        return "button1";
     }
 
 }
